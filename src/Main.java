25570063f8f2ed18de49aba2d28349e014aa59e1
Nghiemducnam <nghiemducnam0312@gmail.com>
2019-11-04 00:02:07 +0700

    ver1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Square [] squares = new Square[3];
+        squares[0] = new Square("red", 3.0,true);
+        squares[1] = new Square(2);
+        squares[2] = new Square();
+
+        for (Square square: squares) {
+            square.howToColor();
+            System.out.println(square);
+        }
+
+    }
+}
